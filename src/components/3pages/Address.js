c6e04ddf736bd38527964d67004b0928783ed76a
abhishek <abhishekbca15.iimtgn@gmail.com>
2020-02-19 23:25:14 +0530

    adding some css editing footer

---
+++ src/components/3pages/Address.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Pages.css';
+
+export default function Address() {
+    return (
+        <div className='address'>
+            <div className='address-heading'>Our Address</div>
+            <div className='address-container'>
+            <h4>E-122, Alpha 1st </h4>
+            <p>Greater Noida (Uttar Pradesh)</p>
+            <p>Pin Code - 201308</p>
+            <h5>India </h5>
+            </div>
+        </div>
+    )
+}
--- src/components/4footer/Footer.js
+++ src/components/4footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Footer.css';
 import SocialLinks from '../3pages/SocialIcons';
+import Address from '../3pages/Address';
 
 export default function Footer() {
     return (
         <footer>
-            <div className='footer-item'></div>
+            <div className='footer-item'>
+                <Address />
+            </div>
             <div className='footer-item'>
                 <SocialLinks />
             </div>
