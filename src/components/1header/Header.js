a9ea218fe31717039478ed2133a29868d479483a
abhishek <abhishekbca15.iimtgn@gmail.com>
2020-02-20 13:40:07 +0530

    changing hamburger

--- src/components/1header/Header.js
+++ src/components/1header/Header.js
@@ -3,15 +3,27 @@ import './Header.css';
 import {NavLink} from 'react-router-dom';
 
 export default class Header extends Component {
+    state= {
+        isOpen : false,
+    }
+
     render() {
         return (
             <div className='header'>
                 <nav>
                     <div className='logo'><NavLink to='/' className='link'>AbhiShek</NavLink></div>
-                    <ul className='navLinks'>
+                    <ul className={this.state.isOpen ? "navLinks" : "navLinks open"}>
                         <li><NavLink to='/about-us' className='link' activeClassName='OnActiveClass'>about-us</NavLink></li>
                         <li><NavLink to='/works' className='link' activeClassName='OnActiveClass'>work</NavLink></li>
                     </ul>
+                    <div 
+                        className='hamburger'
+                        onClick={()=>this.setState({isOpen: !this.state.isOpen})} 
+                    >
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    </div>
                     
                 </nav>
         </div>
