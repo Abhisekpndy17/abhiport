c2454e8272b7ecddde2e21f335625db4e59459c2
abhishek <abhishekbca15.iimtgn@gmail.com>
2020-02-20 12:29:09 +0530

    404 pages add

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -4,7 +4,11 @@ import './Work.css'
 export default function Works() {
     return (
         <div className='work'>
-            Works
+            <div className='work-container'>
+                <h2>404</h2>
+                <h4>Work in progress</h4>
+                <p>Sorry for any inconvenience caused this page is under construction</p>
+            </div>
         </div>
     )
 }
