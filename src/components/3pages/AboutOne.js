54e4070f8e754f870d0929517951ff831e4d96db
abhishek <abhishekbca15.iimtgn@gmail.com>
2020-02-20 11:46:35 +0530

    update the some big in heading section

--- src/components/3pages/AboutOne.js
+++ src/components/3pages/AboutOne.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import './Pages.css';
+import WebImg from '../images/web.png';
+import MobileImg from '../images/phone.png';
+import SettingImg from '../images/web.png';
+import FolderImg from '../images/folder.png';
+
+
 
 export default function AboutOne() {
     return (
         <div className='aboutOne'>
-            <div className='heading-one'>Heading-One</div>
+            <div className='heading-one'>What I have to do</div>
             <div className='oneHead-item'>
                 <div className='item-one'>
-                    <div className=''></div>
-                    <div className=''></div>
+                    <div className='items-one-img'>
+                        <img src={SettingImg} alt='sdfghjk' />
+                    </div>
+                    <div className='items-one-p'>
+                        <h4>Web Development</h4>
+                       <p>able to create a beautiful sites</p>
+                    </div>
                 </div>
-                <div className='item-one'></div>
-                <div className='item-one'></div>
-                <div className='item-one'></div>
+                <div className='item-one'>
+                <div className='items-one-img'>
+                        <img src={FolderImg} alt='sdfghjk' />
+                    </div>
+                    <div className='items-one-p'>
+                        <h4>React Developer</h4>
+                       <p>able to create sites using ReactJS </p>
+                    </div>
+                </div>
+                <div className='item-one'>
+                <div className='items-one-img'>
+                        <img src={MobileImg} alt='sdfghjk' />
+                    </div>
+                    <div className='items-one-p'>
+                        <h4>Responsive design</h4>
+                       <p>responsive sites at a professional level</p>
+                    </div>
+                </div>
+                <div className='item-one'>
+                <div className='items-one-img'>
+                        <img src={WebImg} alt='sdfghjk' />
+                    </div>
+                    <div className='items-one-p'>
+                        <h4>Development</h4>
+                       <p>able to create a beautiful sites</p>
+                    </div>
+                </div>
             </div>
             <div className='right'></div>
         </div>
--- src/components/4footer/Footer.js
+++ src/components/4footer/Footer.js
@@ -7,6 +7,7 @@ import ContactUs from '../3pages/ContactUs';
 export default function Footer() {
     return (
         <footer>
+            <div className='footer'>
             <div className='footer-item'>
                 <Address />
             </div>
@@ -15,6 +16,13 @@ export default function Footer() {
             </div>
             <div className='footer-item'>
             <SocialLinks />
+            </div>
+            </div>
+            <div className='footer-last'>
+                <div className='foot-last-content'>
+                    <p>Design and developed by</p>
+                    <h3>Abhishek Pandey</h3>
+                </div>
             </div>
         </footer>
     )
